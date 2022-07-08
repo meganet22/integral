e2776a42a9ba0a4b925c638137c23509449ede08
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-08 13:36:38 +0300

    Update karsilastirma.js

--- donguler/karsilastirma.js
+++ donguler/karsilastirma.js
@@ -4,6 +4,22 @@ for(var i=0; i<24; i++)
   alert("bugün çok güzel geçecek");
 }
 
+
+//değer göndermek
+function karebulma()
+{
+  for (var i=0; i<10; i++)
+    {
+    karebul (i);
+    }
+    
+    }
+    function karebul(say)
+    {
+  alert("karekök:"+ Math.sqrt(cevap) );
+}
+
+
 //do while
 //while
 //obje veya dizi tipinde //for .. in

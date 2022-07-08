963b8931b982f8a3df0cd1828b97ff78a2bdb775
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-08 15:29:51 +0300

    Update ifelse.js

--- kosullu_yapilar/ifelse.js
+++ kosullu_yapilar/ifelse.js
@@ -31,3 +31,17 @@ if ( a != 5 && true || a) {
 console.log(4);
 console.log(5);
 console.log(6);*/
+
+-
+
+// "hangisiBuyuk" adında bir fonksiyon oluşturun. 
+// Fonksiyon 2 parametre alacaktır
+// Fonksiyon geriye eğer ilk parametre değeri büyükse 1, aksi halde 2 değeri döndürecektir
+
+function hangisiBuyuk(sayi1, sayi2){
+  if(sayi1 > sayi2) return 1;
+  if(sayi2 > sayi1) return 2;
+}
+
+console.log(hangisiBuyuk(9,4));
+console.log(hangisiBuyuk(3,8));

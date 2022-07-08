a2a6509108fe230bfbd7eb7ca9da1e7d1060d64d
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-08 15:38:14 +0300

    Update ifelse.js

--- kosullu_yapilar/ifelse.js
+++ kosullu_yapilar/ifelse.js
@@ -45,3 +45,42 @@ function hangisiBuyuk(sayi1, sayi2){
 
 console.log(hangisiBuyuk(9,4));
 console.log(hangisiBuyuk(3,8));
+
+-
+    
+console.log(' "tekmi" adında bir fonksiyon oluşturun, fonksiyon kendisine verilen 1 sayı değeri eğer tek ise true, değilse false boolean değeri döndürsün ');
+
+function tekmi(sayi){
+  if(sayi % 2 === 1) return true; 
+  else 
+    return false;
+}
+
+console.log(tekmi(5));
+console.log(tekmi(6));
+
+-
+    
+// "hesapMakinesi" adıyla bir fonksiyon oluşturun.
+// Fonksiyon 3 parametre alacaktır
+// 1. parametre string bir değer olacaktır ve şu karakterlerden oluşabilir: + - / *  (artı eksi bölü yıldız)
+// Fonksiyon 2. ve 3. parametrede gelen sayısal değerlere 1. parametrede gelen değere göre işlem uygulayacaktır ve sonucu döndürecektir.
+// Eğer 1. parametre belirtilen 4 karakterden birine eşit değilse geriye false değer döndürecektir.
+
+
+function hesapMakinesi(p1, p2, p3){
+  if( p1 == "-") return p2 - p3;
+  else if(p1 == "*") return p2 * p3;
+  else if(p1=="/") return p2 / p3;
+  else if(p1 == "+") return p2 + p3;
+  else return false;
+}
+
+console.log(hesapMakinesi("-",3,9));
+console.log(hesapMakinesi("*",3,9));
+console.log(hesapMakinesi("/",3,9));
+console.log(hesapMakinesi("+",3,9));
+console.log(hesapMakinesi("b",3, 9));
+
+-
+    

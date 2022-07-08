8ad0f9542fd0df2313651c969ae7c068f80a77bd
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-08 15:55:44 +0300

    Update while.js

--- donguler/while.js
+++ donguler/while.js
@@ -29,3 +29,15 @@ console.log(sayac); //sayac değerini yazdır
 sayac++; //sayac değerini 1 arttır
 }
 -
+0 ile 100 arasında(0 ve 100 hariç) tek sayıları(2'ye bölümünden kalan 1 olmalı) consola yazdıran bir JavaScript programı yazınız.
+console.log("0(dahil) ile 100(dahil) arasında 2'nin katları:");
+let sayac = 0; 
+while ( sayac < 101 ) { 
+let kalan = sayac % 2; 
+if ( kalan === 1 ) { 
+console.log(sayac); 
+}
+sayac++; 
+}
+
+-
